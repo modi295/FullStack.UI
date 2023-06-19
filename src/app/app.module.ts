0a5a0188ef8deba90c261036e62a29a21b5a4893
modi295 <chamanmodi19@gmail.com>
2023-06-19 13:40:02 +0530

    modified:   angular.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/components/employees/add-employee/add-employee.component.css 	new file:   src/app/components/employees/add-employee/add-employee.component.html 	new file:   src/app/components/employees/add-employee/add-employee.component.spec.ts 	new file:   src/app/components/employees/add-employee/add-employee.component.ts 	new file:   src/app/components/employees/edit-employee/edit-employee.component.css 	new file:   src/app/components/employees/edit-employee/edit-employee.component.html 	new file:   src/app/components/employees/edit-employee/edit-employee.component.spec.ts 	new file:   src/app/components/employees/edit-employee/edit-employee.component.ts 	new file:   src/app/components/employees/employees-list/employees-list.component.css 	new file:   src/app/components/employees/employees-list/employees-list.component.html 	new file:   src/app/components/employees/employees-list/employees-list.component.spec.ts 	new file:   src/app/components/employees/employees-list/employees-list.component.ts 	new file:   src/app/models/employee.model.ts 	new file:   src/app/services/employees.service.spec.ts 	new file:   src/app/services/employees.service.ts 	new file:   src/environments/environment.development.ts 	new file:   src/environments/environment.ts 	modified:   src/index.html 	modified:   tsconfig.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
+import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/employees/edit-employee/edit-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesListComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
